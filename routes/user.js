638c40b5c59705cb65ea282797fb5ca04cfad3a4
mohd-rafey-khan <khan123rafey@gmail.com>
2022-08-20 17:00:34 +0530

    modified file structure1

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+/**
+ * @inheritdoc
+ * all user related route handler
+ */
 
 const authController = require('../controller/AuthController');
 router.post('/register',authController.register);
